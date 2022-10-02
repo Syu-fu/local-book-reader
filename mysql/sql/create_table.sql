f4bccb83bd9a1fb1ab205795049fb0766d925be8
Syu fu <syu.fu.dev@gmail.com>
2022-10-02 15:47:40 +0900

    fix(DB): Changed to use UUID for ID

--- mysql/sql/create_table.sql
+++ mysql/sql/create_table.sql
@@ -1,5 +1,4 @@
 CREATE TABLE IF NOT EXISTS `local_book_reader`.`books` (
-    `book_id`        CHAR(32)  NOT NULL,
     `volume`         CHAR(100) NOT NULL,
     `display_order`  INT DEFAULT '100' ,
     `thumbnail`      TEXT(500) NOT NULL,
@@ -9,11 +8,11 @@ CREATE TABLE IF NOT EXISTS `local_book_reader`.`books` (
     `publisher`      TEXT(500) NOT NULL,
     `create_at`      DATETIME  NOT NULL,
     `update_at`      DATETIME  NOT NULL,
+    `book_id`        CHAR(36)    DEFAULT (BIN_TO_UUID(UUID_TO_BIN(UUID(), 1))),
     PRIMARY KEY (`book_id`, `volume`)
 );
 
 CREATE TABLE IF NOT EXISTS `local_book_reader`.`book_groups` (
-    `book_id`        CHAR(32)  NOT NULL,
     `title`          TEXT(500) NOT NULL,
     `title_reading`  TEXT(500) NOT NULL,
     `author`         TEXT(500) NOT NULL,
@@ -22,24 +21,25 @@ CREATE TABLE IF NOT EXISTS `local_book_reader`.`book_groups` (
     `create_at`      DATETIME  NOT NULL,
     `update_at`      DATETIME  NOT NULL,
     PRIMARY KEY (`book_id`)            ,
+    `book_id`        CHAR(36)    NOT NULL,
     FOREIGN KEY (`book_id`)
     REFERENCES `books`(`book_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `local_book_reader`.`tags` (
-    `tag_id`         CHAR(32)  NOT NULL,
     `tag_name`       CHAR(100) NOT NULL,
     `create_at`      DATETIME  NOT NULL,
     `update_at`      DATETIME  NOT NULL,
+    `tag_id`         CHAR(36)    DEFAULT (BIN_TO_UUID(UUID_TO_BIN(UUID(), 1))),
     PRIMARY KEY (`tag_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `local_book_reader`.`tagging` (
-    `tag_id`         CHAR(32)  NOT NULL,
-    `book_id`        CHAR(32)  NOT NULL,
     `create_at`      DATETIME  NOT NULL,
     `update_at`      DATETIME  NOT NULL,
     PRIMARY KEY (`tag_id`, `book_id`)  ,
+    `tag_id`         CHAR(36)    NOT NULL,
+    `book_id`        CHAR(36)    NOT NULL,
     FOREIGN KEY (`book_id`)
     REFERENCES `books`(`book_id`)      ,
     FOREIGN KEY (`tag_id`)
