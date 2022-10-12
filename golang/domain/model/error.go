56e67a5c260e43adba9eda7f06489cd0e4e04431
Syu fu <syu.fu.dev@gmail.com>
2022-10-12 21:40:05 +0900

    feat(golang): Add error 'tag already tagging'

--- golang/domain/model/error.go
+++ golang/domain/model/error.go
@@ -10,3 +10,6 @@ var ErrInvalidEntity = errors.New("Invalid entity")
 
 //ErrCannotBeDeleted cannot be deleted
 var ErrCannotBeDeleted = errors.New("Cannot Be Deleted")
+
+//ErrTagAlreadyTagging cannot tagging
+var ErrTagAlreadyTagging = errors.New("Tag already tagging")
