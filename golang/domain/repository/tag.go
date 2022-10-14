b63862e01bf6f7cbf6a7ff2372208165098b8c77
Syu fu <syu.fu.dev@gmail.com>
2022-10-14 21:48:14 +0900

    feat(golang): Add a usecase for tag

---
+++ golang/domain/repository/tag.go
@@ -0,0 +1,14 @@
+package repository
+
+import (
+	"local-book-reader/domain/model"
+)
+
+//go:generate mockhandler -source=$GOFILE -destination=./mock/$GOFILE -package=mock
+type TagRepository interface {
+	Read() (tags []*model.Tag, err error)
+	ReadById(id string) (tag *model.Tag, err error)
+	Create(tag *model.Tag) (*model.Tag, error)
+	Update(tag *model.Tag) (*model.Tag, error)
+	Delete(id string) (string, error)
+}
