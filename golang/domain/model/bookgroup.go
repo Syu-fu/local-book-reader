fa9c3de42ed89f72e656cc9058e3c499eaab26cf
Syu fu <syu.fu.dev@gmail.com>
2022-10-20 22:41:11 +0900

    feat(golang): Add a domain model for bookgroup

---
+++ golang/domain/model/bookgroup.go
@@ -0,0 +1,68 @@
+package model
+
+// bookgroup is BookGroupModel
+type BookGroup struct {
+	BookId        string
+	Title         string
+	TitleReading  string
+	Author        string
+	AuthorReading string
+	Thumbnail     string
+	CreateAt      string
+	UpdateAt      string
+	Tags          []*Tag
+}
+
+// NewBookGroup create a new bookgroup
+func NewBookGroup(bookId, title, titleReading, author, authorReading, thumbnail string) (*BookGroup, error) {
+	bg := &BookGroup{
+		BookId:        bookId,
+		Title:         title,
+		TitleReading:  titleReading,
+		Author:        author,
+		AuthorReading: authorReading,
+		Thumbnail:     thumbnail,
+	}
+	err := bg.Validate()
+	if err != nil {
+		return nil, err
+	}
+	return bg, nil
+}
+
+func (bg *BookGroup) Validate() error {
+	if bg.BookId == "" || bg.Title == "" {
+		return ErrInvalidEntity
+	}
+	return nil
+}
+
+func (bg *BookGroup) GetTag(tagId string) (*Tag, error) {
+	for _, v := range bg.Tags {
+		if v.TagId == tagId {
+			return v, nil
+		}
+	}
+	return nil, ErrNotFound
+}
+
+// AddTag add a tag
+func (bg *BookGroup) AddTag(tag *Tag) error {
+	_, err := bg.GetTag(tag.TagId)
+	if err == nil {
+		return ErrTagAlreadyTagging
+	}
+	bg.Tags = append(bg.Tags, tag)
+	return nil
+}
+
+// RemoveTag remove a tag
+func (bg *BookGroup) RemoveTag(tagId string) error {
+	for i, j := range bg.Tags {
+		if j.TagId == tagId {
+			bg.Tags = append(bg.Tags[:i], bg.Tags[i+1:]...)
+			return nil
+		}
+	}
+	return ErrNotFound
+}
