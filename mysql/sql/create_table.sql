2e03b3c2ba590e79b4511baadb13900f12b63057
Syu fu <syu.fu.dev@gmail.com>
2022-10-22 17:40:29 +0900

    fix(DB): Fix DB key dependencies

--- mysql/sql/create_table.sql
+++ mysql/sql/create_table.sql
@@ -1,3 +1,15 @@
+CREATE TABLE IF NOT EXISTS `local_book_reader`.`book_groups` (
+    `book_id`        CHAR(36)    NOT NULL,
+    `title`          TEXT(500)   NOT NULL,
+    `title_reading`  TEXT(500)   NOT NULL,
+    `author`         TEXT(500)   NOT NULL,
+    `author_reading` TEXT(500)   NOT NULL,
+    `thumbnail`      TEXT(500)   NOT NULL,
+    `created_at`     DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    `updated_at`     DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+    PRIMARY KEY (`book_id`)
+);
+
 CREATE TABLE IF NOT EXISTS `local_book_reader`.`books` (
     `book_id`        CHAR(36)    DEFAULT (BIN_TO_UUID(UUID_TO_BIN(UUID(), 1))),
     `volume`         CHAR(100)   NOT NULL,
@@ -9,21 +21,9 @@ CREATE TABLE IF NOT EXISTS `local_book_reader`.`books` (
     `publisher`      TEXT(500)   NOT NULL,
     `created_at`     DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
     `updated_at`     DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-    PRIMARY KEY (`book_id`, `volume`)
-);
-
-CREATE TABLE IF NOT EXISTS `local_book_reader`.`book_groups` (
-    `book_id`        CHAR(36)    NOT NULL,
-    `title`          TEXT(500)   NOT NULL,
-    `title_reading`  TEXT(500)   NOT NULL,
-    `author`         TEXT(500)   NOT NULL,
-    `author_reading` TEXT(500)   NOT NULL,
-    `thumbnail`      TEXT(500)   NOT NULL,
-    `created_at`     DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-    `updated_at`     DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-    PRIMARY KEY (`book_id`),
+    PRIMARY KEY (`book_id`, `volume`),
     FOREIGN KEY (`book_id`)
-    REFERENCES `books`(`book_id`)
+    REFERENCES `book_groups`(`book_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `local_book_reader`.`tags` (
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `local_book_reader`.`tagging` (
     `updated_at`     DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
     PRIMARY KEY (`tag_id`, `book_id`),
     FOREIGN KEY (`book_id`)
-    REFERENCES `books`(`book_id`),
+    REFERENCES `book_groups`(`book_id`),
     FOREIGN KEY (`tag_id`)
     REFERENCES `tags`(`tag_id`)
 );
