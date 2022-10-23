9018b08193068c8973d03a0d65400fbc755823ff
Syu fu <syu.fu.dev@gmail.com>
2022-10-23 21:18:43 +0900

    feat(golang): Add search to DB Repository

--- golang/domain/repository/bookGroup.go
+++ golang/domain/repository/bookGroup.go
@@ -6,6 +6,7 @@ import (
 
 type BookGroupRepository interface {
 	Read() (tags []*model.BookGroup, err error)
+	Search(word string) (tags []*model.BookGroup, err error)
 	ReadById(id string) (tag *model.BookGroup, err error)
 	Create(tag *model.BookGroup) (*model.BookGroup, error)
 	Update(tag *model.BookGroup) (*model.BookGroup, error)
