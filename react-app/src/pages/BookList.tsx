72995bb5157bdc67037030206395bd6521086699
Syu fu <syu.fu.dev@gmail.com>
2022-10-29 23:16:00 +0900

    feat(react): Add book list page

---
+++ react-app/src/pages/BookList.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react'; import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import { useParams } from 'react-router-dom'
+import ResponsiveDrawer from '../components/ResponsiveDrawer';
+import BookListItem from '../components/BookListItem'
+import { useFetchBooks } from '../hooks/useFetchBook'
+
+const BookList: React.FC = () => {
+  const params = useParams<{ bookId: string }>()
+  console.log(params.bookId)
+
+  const { data, error, loading } = useFetchBooks(params.bookId)
+
+  return (
+    <ResponsiveDrawer>
+      <List
+        sx={{
+          width: '100%',
+          bgcolor: 'background.paper',
+        }}
+      >
+        <Divider variant="inset" component="li" />
+        {data && data.map((book) => {return (
+          <List
+            sx={{
+              width: '100%',
+              bgcolor: 'background.paper',
+            }}
+          >
+            <BookListItem
+              src={book.thumbnail}
+              title={book.title}
+              author={book.author}
+              volume={book.volume}
+            />
+            <Divider variant="inset" component="li" />
+          </List>
+        )})}
+      </List>
+      {error && <div>{error.message}</div>}
+      {loading && <div>...loading</div>}
+    </ResponsiveDrawer>
+  )
+}
+export default BookList;
