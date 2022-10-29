2ae68c5777bac9bcb52507be58cd8cdfe1a6b2ef
Syu fu <syu.fu.dev@gmail.com>
2022-10-29 23:22:19 +0900

    fix(golang): Fix Book and BookGroup's presenter typo

--- golang/handler/presenter/book.go
+++ golang/handler/presenter/book.go
@@ -4,7 +4,7 @@ type Book struct {
 	BookId       string `json:"bookId"`
 	Volume       string `json:"volume"`
 	DisplayOrder int    `json:"displayOrder"`
-	Thumbnail    string `json:"thumnail"`
+	Thumbnail    string `json:"thumbnail"`
 	Title        string `json:"title"`
 	Filepath     string `json:"filepath"`
 	Author       string `json:"author"`
