0f1d156b30823e3cc1d2d80df39ee13a53e537bb
Syu fu <syu.fu.dev@gmail.com>
2022-11-07 22:48:25 +0900

    feat(react): lib with config file support

--- react-app/src/lib/axios.ts
+++ react-app/src/lib/axios.ts
@@ -1,7 +1,8 @@
 import Axios from 'axios'
+import { API_PORT, IPADDRESS } from '../config/index'
 
 const axios = Axios.create({
-  baseURL: 'http://localhost:8000'
+  baseURL: `http://${IPADDRESS}:${API_PORT}`
 })
 
 export default axios
--- react-app/src/lib/mc.ts
+++ react-app/src/lib/mc.ts
@@ -1,11 +1,12 @@
 import * as minio from 'minio'
+import { IPADDRESS, MINIO_PORT, MINIO_ROOT_USER, MINIO_ROOT_PASS } from '../config/index'
 
 const mc = new minio.Client({
-  endPoint: "localhost",
-  port: 9000,
+  endPoint: IPADDRESS,
+  port: MINIO_PORT,
   useSSL: false,
-  accessKey: "admin",
-  secretKey: "administrator"
+  accessKey: MINIO_ROOT_USER,
+  secretKey: MINIO_ROOT_PASS
 })
 
 export default mc
