37fbe5031128eb9fdf79dc4f62df133d626815b4
Syu fu <syu.fu.dev@gmail.com>
2022-11-23 21:04:05 +0900

    breaking(DB): Delete thumbnail and filepath

--- mysql/sql/create_table.sql
+++ mysql/sql/create_table.sql
@@ -4,7 +4,6 @@ CREATE TABLE IF NOT EXISTS `local_book_reader`.`book_groups` (
     `title_reading`  TEXT(500)   NOT NULL,
     `author`         TEXT(500)   NOT NULL,
     `author_reading` TEXT(500)   NOT NULL,
-    `thumbnail`      TEXT(500)   NOT NULL,
     `created_at`     DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
     `updated_at`     DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
     PRIMARY KEY (`book_id`)
@@ -14,9 +13,7 @@ CREATE TABLE IF NOT EXISTS `local_book_reader`.`books` (
     `book_id`        CHAR(36)    DEFAULT (BIN_TO_UUID(UUID_TO_BIN(UUID(), 1))),
     `volume`         CHAR(100)   NOT NULL,
     `display_order`  INT         DEFAULT '100',
-    `thumbnail`      TEXT(500)   NOT NULL,
     `title`          TEXT(500)   NOT NULL,
-    `filepath`       TEXT(500)   NOT NULL,
     `author`         TEXT(500)   NOT NULL,
     `publisher`      TEXT(500)   NOT NULL,
     `direction`      CHAR(3)     NOT NULL,
